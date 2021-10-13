ced150f95eb411bb96269e587bd1db7653eaa258
JonathanAmarall <jhouamaral95@gmail.com>
2021-10-13 00:35:10 -0300

    validado email

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,7 +1,12 @@
 import { Flex } from '@chakra-ui/layout';
 import { Input } from '@chakra-ui/react';
+import { useState, useRef } from 'react';
 
 export function SearchBox() {
+  // const [search, setSearch] = useState('');
+  // const searchInputRef = useRef<HTMLInputElement>(null);
+  // searchInputRef.current.value;
+
   return (
     <Flex
       as="label"
@@ -17,12 +22,15 @@ export function SearchBox() {
       borderRadius="full"
     >
       <Input
+        // ref={searchInputRef}
         color="gray.50"
         variant="unstyled"
         paddingX="4"
         marginRight="4"
         placeholder="Buscar na plataforma"
         _placeholder={{ color: 'gray.400' }}
+        // value={search}
+        // onChange={(event) => setSearch(event.target.value)}
       />
     </Flex>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,36 @@
 import { Flex } from '@chakra-ui/layout';
 import { Button, Stack, FormControl } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { Input } from '../components/Form/Input';
 
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória'),
+});
+
 export default function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values, event) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
     <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
       <Flex
@@ -13,16 +41,35 @@ export default function SignIn() {
         p="8"
         borderRadius="8px"
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
           <FormControl>
-            <Input name="email" type="text" label="E-mail" />
+            <Input
+              name="email"
+              type="text"
+              label="E-mail"
+              error={errors.email}
+              {...register('email')}
+            />
           </FormControl>
           <FormControl>
-            <Input name="password" type="text" label="Password" />
+            <Input
+              name="password"
+              type="password"
+              label="Password"
+              error={errors.password}
+              {...register('password')}
+            />
           </FormControl>
         </Stack>
-        <Button type="submit" mt="6" colorScheme="purple" size="lg">
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="purple"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
