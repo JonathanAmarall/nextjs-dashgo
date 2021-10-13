462c0b171ecd1595f3ce8109833f467cd8e24ae2
JonathanAmarall <jhouamaral95@gmail.com>
2021-10-13 16:33:51 -0300

    refact: separdesacoplado metodo que busca usuarios da query

--- src/services/hooks/useUsers.tsx
+++ src/services/hooks/useUsers.tsx
@@ -1,28 +1,26 @@
 import { useQuery } from 'react-query';
 import { api } from '../../services/api';
 
-export function useUsers() {
-  return useQuery(
-    'users',
-    async () => {
-      const { data } = await api.get<any>('users');
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString('pt-br', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric',
-          }),
-        };
-      });
+export async function getUsers() {
+  const { data } = await api.get<any>('users');
+  const users = data.users.map((user) => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-br', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+    };
+  });
 
-      return users;
-    },
-    {
-      staleTime: 1000 * 5,
-    }
-  );
+  return users;
+}
+
+export function useUsers() {
+  return useQuery('users', getUsers, {
+    staleTime: 1000 * 5,
+  });
 }
