21b279d56ee956aea6927b38bc808adbd3245957
JonathanAmarall <jhouamaral95@gmail.com>
2021-10-13 14:43:19 -0300

    feat: adicionado miragejs para e configurado faker para dados teste

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import { SideBarDrawerProvider } from '../contexts/SidebarDrawerContext';
+import { makeServer } from '../services/mirage';
 
 import '../styles/global.scss';
 import { theme } from '../styles/theme';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -21,12 +21,20 @@ import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header/Index';
 import { Sidebar } from '../../components/Sidebar';
 import { Pagination } from '../../components/Pagination';
+import { useEffect } from 'react';
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
   const isLoading = true;
   const isFetching = false;
 
